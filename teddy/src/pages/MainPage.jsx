bd6cea21348f61247d8e9ffec8f1fc29a94ae73d
yashas-salankimatt <yashas.salankimatt@gmail.com>
2021-07-08 05:18:44 -0400

    Update MainPage on Login
    Now hides or shows the rest of the UI based on whether there is a logged in user or not.

--- teddy/src/pages/MainPage.jsx
+++ teddy/src/pages/MainPage.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './MainPage.css'
 import Navbar from '../components/navbar';
 import BucketView from '../components/bucketView';
 import CalendarView from '../components/calendarView';
+import { UserContext } from '../providers/UserProvider';
 
 function MainPage(props) {
+    const user = useContext(UserContext);
+
     return (
         <div>
             <div className="Navbar">
                 <Navbar></Navbar>
             </div>
-            <div className="MainViewWrapper">
+            {user && <div className="MainViewWrapper">
                 <div className="Buckets">
                     <BucketView></BucketView>
                 </div>
                 <div className="CalendarView">
                     <CalendarView></CalendarView>
                 </div>
-            </div>
+            </div>}
         </div>
     );
 }
