773066eea11d6578ca74a20527e230cd33937a2b
yashas-salankimatt <yashas.salankimatt@gmail.com>
2021-07-11 23:49:19 -0400

    Edit project popup v1 completed
    No bug testing has been done tho

--- teddy/src/components/EditProjectPopup.jsx
+++ teddy/src/components/EditProjectPopup.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { fb } from '../utils/FirebaseConfig';
 import './EditProjectPopup.css';
 
 function EditProjectPopup({trigger=false, projData, setTrig, updateParentData}) {
@@ -8,13 +9,36 @@ function EditProjectPopup({trigger=false, projData, setTrig, updateParentData}) 
     const [description, setDescription] = useState('');
     const [dueDate, setDueDate] = useState();
 
+
+    const [dueDateStr, setDueDateStr] = useState();
+    const [dueTimeStr, setDueTimeStr] = useState();
+
     useEffect(() => {
         setProjDocData(projData.element);
         setName(projData.element.projectName);
         setCompleted(projData.element.completed);
         setDescription(projData.element.description);
         setDueDate(projData.element.dueDate);
-        console.log(projData.element.dueDate);
+        
+        var year = projData.element.dueDate.toDate().getFullYear().toString();
+        var month = projData.element.dueDate.toDate().getMonth().toString();
+        if (month.length === 1) {
+            month = '0' + month;
+        }
+        var day = projData.element.dueDate.toDate().getDate().toString();
+        if (day.length === 1){
+            day = '0' + day;
+        }
+        setDueDateStr(year+'-'+month+'-'+day);
+        var hours = projData.element.dueDate.toDate().getHours().toString();
+        if (hours.length === 1){
+            hours = '0' + hours;
+        }
+        var mins = projData.element.dueDate.toDate().getMinutes().toString();
+        if (mins.length === 1){
+            mins = '0' + mins;
+        }
+        setDueTimeStr(hours+':'+mins);
     }, []);
 
     const updateProject = () => {
@@ -30,11 +54,17 @@ function EditProjectPopup({trigger=false, projData, setTrig, updateParentData}) 
         setTrig(false);
     };
 
+    useEffect(() => {
+        var date = new Date(dueDateStr+'T'+dueTimeStr+':00');
+        // console.log(date);
+        setDueDate(fb.firestore.Timestamp.fromDate(date));
+    }, [dueDateStr, dueTimeStr]);
+
     return (trigger) ? (
         <div className='popup'>
             <div className='popup-inner'>
                 <button className='close-btn btn btn-secondary' onClick={() => {setTrig(false)}}>Close</button>
-                <h3>Eid Project: {projDocData.projectName}</h3>
+                <h3>Edit Project: {projDocData.projectName}</h3>
                 <div className='EditableField'>
                     <h5 className='m-1'>Project Name: </h5>
                     <form>
@@ -56,8 +86,8 @@ function EditProjectPopup({trigger=false, projData, setTrig, updateParentData}) 
                 <div className='EditableField'>
                     <h5 className='m-1'>Due Date: </h5>
                     <form>
-                        <input type='date'></input>
-                        <input type='time'></input>
+                        <input type='date' defaultValue={dueDateStr} onChange={(event) => {setDueDateStr(event.target.value)}}></input>
+                        <input type='time' defaultValue={dueTimeStr} onChange={(event) => {setDueTimeStr(event.target.value)}}></input>
                     </form>
                 </div>
                 <button className='btn btn-secondary' onClick={() => {updateProject()}}>Save Changes</button>
